a04979651fbb900294ffaa09274b2be2ed5e0a7a
Seth Ozaki <162648376+Seth-Ozaki@users.noreply.github.com>
2024-08-13 16:42:34 -0600

    Likes grading and time conversion algos

--- TS/practice.ts
+++ TS/practice.ts
@@ -47,17 +47,86 @@ function timeConversion(time: string): string {
     return military;
 }
 
-const testTimeConversion = timeConversion("12:00:00AM");
-console.log(testTimeConversion);
+// const testTimeConversion = timeConversion("12:00:00AM");
+// console.log(testTimeConversion);
 
-const testTimeConversion1 = timeConversion("04:00:00AM");
-console.log(testTimeConversion1);
+// const testTimeConversion1 = timeConversion("04:00:00AM");
+// console.log(testTimeConversion1);
 
-const testTimeConversion2 = timeConversion("12:00:00PM");
-console.log(testTimeConversion2);
+// const testTimeConversion2 = timeConversion("12:00:00PM");
+// console.log(testTimeConversion2);
 
-const testTimeConversion3 = timeConversion("4:00:00PM");
-console.log(testTimeConversion3);
+// const testTimeConversion3 = timeConversion("4:00:00PM");
+// console.log(testTimeConversion3);
 
 // ------------------------------------------------------------
+// HackerLand University has the following grading policy:
+// Every student receives a grade in the inclusive range from 0 to 100.
+// Any grade less than 40 is a failing grade.
+// Sam is a professor at the university and likes to round each student's  according to these rules:
+// If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+// If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
 
+const grading = (grades: number[]): number[] => {
+
+    for (let i = 0; i < grades.length; i++) {
+        let n = grades[i] % 10; //grabs the second digit of the grade
+        if (grades[i] < 38) {
+            continue;
+        }
+        if (n === 3 || n === 8) {
+            grades[i] += 2;
+        } else if (n === 4 || n === 9) {
+            grades[i] += 1;
+        }
+    }
+    return grades;
+};
+
+// const testGrading = grading([84, 43, 88, 79, 33]);
+// console.log(testGrading);
+
+// ------------------------------------------------------------
+// You probably know the "like" system from Facebook and other pages.
+// People can "like" blog posts, pictures or other items.
+//  We want to create the text that should be displayed next to such an item.
+// Implement the function which takes an array containing the names of people that like an item.
+// It must return the display text as shown in the examples:
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+// Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+const likes = (names: string[]): any => {
+    switch (names.length) {
+        case 0:
+            return "no one likes this";
+        case 1:
+            return `${names[0]} likes this`;
+        case 2:
+            return `${names[0]} and ${names[1]} likes this`;
+        case 3:
+            return `${names[0]}, ${names[1]} and ${names[2]} likes this`;
+        default:
+            return `${names[0]}, ${names[1]} and ${names.length - 2} others likes this`;
+    }
+};
+
+// const testLikes = likes([]);
+// console.log(testLikes);
+
+// const testLikes1 = likes(["Peter"]);
+// console.log(testLikes1);
+
+// const testLikes2 = likes(["Jacob", "Alex"]);
+// console.log(testLikes2);
+
+// const testLikes3 = likes(["Max", "John", "Mark"]);
+// console.log(testLikes3);
+
+// const testLikes4 = likes(["Alex", "Jacob", "Mark", "Max", "Alex", "Jacob", "Mark", "Max"]);
+// console.log(testLikes4);
+
+// ------------------------------------------------------------
