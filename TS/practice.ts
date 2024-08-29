206823b7d45782af697f6221ecfdc49923a5e711
Seth Ozaki <162648376+Seth-Ozaki@users.noreply.github.com>
2024-08-29 11:27:17 -0600

    Morning algos in practice.ts

--- TS/practice.ts
+++ TS/practice.ts
@@ -190,5 +190,80 @@ const kangaroos = (x1: number, v1: number, x2: number, v2: number): any => {
 // console.log(testKangaroos1);
 
 // ------------------------------------------------------------
+// Maria plays college basketball and wants to go pro. Each season she maintains a record of her play.
+// She tabulates the number of times she breaks her season record for most points and least points in a game. 
+// Points scored in the first game establish her record for the season, and she begins counting from there.
+// Example
+// Scores = [12,24,10,24]
+// Scores are in the same order as the games played. She tabulates her results as follows:
+//                                      Count
+//     Game  Score  Minimum  Maximum   Min Max
+//      0      12     12       12       0   0
+//      1      24     12       24       0   1
+//      2      10     10       24       1   1
+//      3      24     10       24       1   1
+// Given the scores for a season, determine the number of times Maria breaks her records for most and least points scored during the season.
+// Return an array with the numbers of times she broke her records. Index 0 is for breaking most points records, and index 1 is for breaking least points records.
 
+const recordBreaker = (arr: number[]): number[] => {
+    let maxScore = arr[0];
+    let minScore = arr[0];
+    let maxMin = [0, 0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > maxScore) {
+            maxScore = arr[i];
+            maxMin[0]++;
+        }
+        if (arr[i] < minScore) {
+            minScore = arr[i];
+            maxMin[1]++;
+        }
+    }
+    return maxMin;
+};
 
+// const testRecordBreaker = recordBreaker([12, 24, 10, 24]);
+// console.log(testRecordBreaker)
+
+// ------------------------------------------------------------
+// Two children, Lily and Ron, want to share a chocolate bar. Each of the squares has an integer on it.
+// Lily decides to share a contiguous segment of the bar selected such that:
+// The length of the segment matches Ron's birth month, and,
+// The sum of the integers on the squares is equal to his birth day.
+// Determine how many ways she can divide the chocolate.
+// Example
+// s = [2,2,1,3,2]
+// d = 4
+// m = 2
+// Lily wants to find segments summing to Ron's birth day, d = 4 with a length equalling his birth month, m = 2. 
+// In this case, there are two segments meeting her criteria: [2,2] and [1,3].
+
+const chocolate = (s: number[], d: number, m: number): number => {
+    let output = 0;
+    let count = 0;
+    let i = 0;
+    let x = 0;
+    let j = 0;
+    while (i < s.length) {
+        count += s[x];
+        j++;
+        x++;
+        if (j === m && count === d) {
+            output++;
+            i++;
+            j = 0;
+            x = i;
+            count = 0;
+        }
+        if (j === m && count !== d) {
+            i++;
+            j = 0;
+            x = i;
+            count = 0;
+        }
+    }
+    return output;
+};
+
+// let testChocolate = chocolate([2, 2, 1, 3, 2], 4, 2);
+// console.log(testChocolate);
